78bf3048f572cc51efab65bf26adbf928ff4e77e
qiaofuyo <1103351140@qq.com>
2025-06-05 13:37:40 +0800

    add: injectable 配置

---
+++ lib/core/di/injection.dart
@@ -0,0 +1,11 @@
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+// 下面这个文件会由 build_runner 自动生成
+import 'injection.config.dart';
+
+/// 全局单例 GetIt 实例
+final GetIt getIt = GetIt.instance;
+
+/// 运行此方法即可注册所有标注了 @injectable 的类型
+@InjectableInit()
+void configureDependencies() => getIt.init();
