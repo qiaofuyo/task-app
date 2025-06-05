ddd5094ef864b1527403f90a28c905b034507dfb
qiaofuyo <1103351140@qq.com>
2025-06-05 22:15:28 +0800

    add: 配置本地数据库与 DI 绑定

---
+++ lib/core/di/database_module.dart
@@ -0,0 +1,34 @@
+import 'package:injectable/injectable.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+
+const String _tasksTable = 'tasks';
+
+@module
+abstract class DatabaseModule {
+  @Named('AppDatabase')
+  @preResolve
+  Future<Database> get database async {
+    final docsDir = await getApplicationDocumentsDirectory();
+    final dbPath = join(docsDir.path, 'app.db');
+
+    return openDatabase(
+      dbPath,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE $_tasksTable (
+            id TEXT PRIMARY KEY,
+            title TEXT NOT NULL,
+            description TEXT NOT NULL,
+            quadrant TEXT NOT NULL,
+            completed INTEGER NOT NULL,
+            createdAt INTEGER NOT NULL
+          )
+        ''');
+      },
+    );
+  }
+}
--- lib/core/di/injection.config.dart
+++ lib/core/di/injection.config.dart
@@ -11,22 +11,33 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
+import 'package:sqflite/sqflite.dart' as _i779;
 import 'package:task_app/application/usecases/transcribe_and_create_task_usecase.dart'
     as _i149;
+import 'package:task_app/core/di/database_module.dart' as _i574;
+import 'package:task_app/data/repositories/local_task_repository.dart' as _i779;
 import 'package:task_app/domain/services/i_task_repository.dart' as _i67;
 import 'package:task_app/domain/services/i_transcription_engine.dart' as _i662;
 
 extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
-  _i174.GetIt init({
+  Future<_i174.GetIt> init({
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
-  }) {
+  }) async {
     final gh = _i526.GetItHelper(
       this,
       environment,
       environmentFilter,
     );
+    final databaseModule = _$DatabaseModule();
+    await gh.factoryAsync<_i779.Database>(
+      () => databaseModule.database,
+      instanceName: 'AppDatabase',
+      preResolve: true,
+    );
+    gh.lazySingleton<_i67.ITaskRepository>(() => _i779.LocalTaskRepository(
+        gh<_i779.Database>(instanceName: 'AppDatabase')));
     gh.factory<_i149.TranscribeAndCreateTaskUseCase>(
         () => _i149.TranscribeAndCreateTaskUseCase(
               gh<_i662.ITranscriptionEngine>(),
@@ -35,3 +46,5 @@ extension GetItInjectableX on _i174.GetIt {
     return this;
   }
 }
+
+class _$DatabaseModule extends _i574.DatabaseModule {}
